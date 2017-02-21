d46dc49d5ec32fcd2501a8b939a02d72cc665d01
Giang Nguyen <luckytintinnz@yahoo.com>
2017-02-22 01:42:37 +1300

    chapter 7 mid

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {
   AppRegistry,
+  View,
 } from 'react-native';
 
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 export default function App() {
-  return <Header />;
+  return (<View>
+    <Header headerText="Albums" />
+    <AlbumList />
+  </View>);
 }
 
 // Render to device
